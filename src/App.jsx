2916c2635900b9182e4ad58016bc87aad26e0867
Jaime Benitez Lope <159242343+jaimebenitezlope@users.noreply.github.com>
2024-05-31 12:22:07 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { GlobalStyles } from './styles/GlobalStyles';
+
+const App = () => {
+	return (
+		<>
+			<GlobalStyles />
+			<h1>Núcleo de la aplicación</h1>
+		</>
+	);
+};
+
+export default App;
