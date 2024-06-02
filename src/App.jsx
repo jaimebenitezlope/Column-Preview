bda2660987a83d96a567cf6eb8513bed95855514
Jaime Benitez Lope <bl.jaime@icloud.com>
2024-06-02 19:14:07 +0200

    the end

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,24 @@ const App = () => {
 			<Cards>
 				<Card
 					icon='sedan'
-					title='Sedan'
+					title='SEDANS'
 					text='Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.'
 					color='orange'
+					position='first'
+				/>
+				<Card
+					icon='suv'
+					title='SUVS'
+					text='Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.'
+					color='blue'
+					position='second'
+				/>
+				<Card
+					icon='luxury'
+					title='LUXURY'
+					text='Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style. '
+					color='green'
+					position='third'
 				/>
 			</Cards>
 		</>
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,12 +1,12 @@
 import style from './card.module.css';
 
-const Card = ({ icon, title, text, color }) => {
+const Card = ({ icon, title, text, color, position }) => {
 	return (
-		<div className={style[color]}>
+		<div className={`${style.card} ${style[color]} ${style[position]}`}>
 			<img src={`/images/${icon}.svg`} alt={`${icon} icon`} />
-			<h2>{title}</h2>
+			<h2 className={style.title}>{title}</h2>
 			<p>{text}</p>
-			<button className=''>Learn More</button>
+			<button style={{ color: `var(--${color})` }}>Learn More</button>
 		</div>
 	);
 };
